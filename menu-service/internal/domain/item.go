8ca49f04313a5d6d79fb1a083e6f5645e78ae9f8
Ricardo Lüders <rluders@redhat.com>
2024-01-02 13:01:22 +0100

    style: clean commented code

--- menu-service/internal/domain/item.go
+++ menu-service/internal/domain/item.go
@@ -14,30 +14,3 @@ type Item struct {
 	UpdatedAt   time.Time   `json:"updated_at" db:"updated_at"`
 	DeletedAt   *time.Time  `json:"deleted_at,omitempty" db:"deleted_at"`
 }
-
-//
-//type Categories []*Category
-//
-//func (c *Categories) Scan(val interface{}) error {
-//	var raw []byte
-//
-//	switch v := val.(type) {
-//	case []byte: // []uint8
-//		raw = v
-//	case string:
-//		raw = []byte(v)
-//	default:
-//		return errors.New(fmt.Sprintf("unsupported type: %T", v))
-//	}
-//
-//	err := json.Unmarshal(raw, &c)
-//	if err != nil {
-//		log.Printf("fail to unmarshall as byte (%T): %v", v, err)
-//		return err
-//	}
-//	return nil
-//}
-//
-//func (c *Categories) Value() (driver.Value, error) {
-//	return json.Marshal(&c)
-//}
